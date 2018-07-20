8fda8cae7f41f09b4fd1ef5637f203d1ec00be0f
chentsulin <chentsulin@gmail.com>
2018-07-20 14:59:49 +0800

    trigger lifecycle methods

--- exampes/basic.js
+++ exampes/basic.js
@@ -14,22 +14,37 @@ const config = {
       on: {
         TIMER: 'yellow',
       },
+      onEntry: 'enterGreen',
+      onExit: 'leaveGreen',
     },
     yellow: {
       on: {
         TIMER: 'red',
       },
+      onEntry: 'enterYellow',
+      onExit: 'leaveYellow',
     },
     red: {
       on: {
         TIMER: 'green',
       },
+      onEntry: 'enterRed',
+      onExit: 'leaveRed',
     },
   },
 };
 
 const mapContextToXStateEvent = () => 'TIMER';
 
-bot.onEvent(middleware([bottenderXState(config, mapContextToXStateEvent)]));
+const actions = {
+  enterGreen: context => context.sendText('enter green'),
+  enterYellow: context => context.sendText('enter yellow'),
+  enterRed: context => context.sendText('enter red'),
+  leaveGreen: context => context.sendText('leave green'),
+  leaveYellow: context => context.sendText('leave yellow'),
+  leaveRed: context => context.sendText('leave red'),
+};
+
+bot.onEvent(middleware([bottenderXState(config, mapContextToXStateEvent, actions)]));
 
 bot.createRuntime();
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 const { Machine } = require('xstate');
 
-function bottenderXState(config, mapContextToXStateEvent) {
+
+function bottenderXState(config, mapContextToXStateEvent, actions) {
   return async (context, next) => {
     const machine = Machine(config);
 
     const currentState = context.state.machine || config.initial;
     const event = await mapContextToXStateEvent(context);
 
-    const nextState = machine.transition(currentState, event).value;
+    const nextState = machine.transition(currentState, event);
+
+    const triggerdActions = nextState.actions;
+
+    for (triggerdAction of triggerdActions) {
+      if (typeof actions[triggerdAction] === 'function') {
+        await actions[triggerdAction](context);
+      } else {
+        warning(
+          false,
+          `${actions[triggerdAction]} is missing in actions`
+        );
+      }
+    }
 
     await next();
 
-    context.setState({ machine: nextState });
+    // FIXME: where?
+    context.setState({ machine: nextState.value });
   };
 }
 
